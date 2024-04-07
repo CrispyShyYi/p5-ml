a5def7cc8d6f9a58032808afe6b57e12ed5ff57f
alfred <alfredwu@umich.edu>
2024-04-07 15:44:45 -0400

    Map.hpp & test cases

---
+++ Map_tests.cpp
@@ -0,0 +1,20 @@
+#include "Map.hpp"
+#include <string>
+#include <vector>
+#include <iostream>
+#include "unit_test_framework.hpp"
+
+using std::pair;
+using std::string;
+using std::vector;
+
+TEST(test_empty) {
+    Map<string, double> words;
+    ASSERT_TRUE(words.empty());
+    ASSERT_TRUE(words.size() == 0);
+    ASSERT_TRUE(words.find("pi") == words.end());
+}
+
+
+
+TEST_MAIN()
